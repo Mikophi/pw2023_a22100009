2eca4998aad7b2c7ba45c5d3047e8b381e2313b4
Agung Fajar Risnandar <a22100009@mhs.stmik-sumedang.ac.id>
2023-04-18 09:37:28 +0700

    menambahkan 1 db

---
+++ kuliah/pertemuan7/mie_gacoan.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 18, 2023 at 04:30 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mie_gacoan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gacoan_menu`
+--
+
+CREATE TABLE `gacoan_menu` (
+  `id_order` int(10) NOT NULL,
+  `menu` varchar(25) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `deskripsi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `gacoan_menu`
+--
+ALTER TABLE `gacoan_menu`
+  ADD PRIMARY KEY (`id_order`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
